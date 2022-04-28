07f40129b1898db3cef8a5233c2531c0d5f72602
Mats Hemå <89969770+mhema42@users.noreply.github.com>
2022-04-28 12:37:17 +0200

    add hero on mainpage with image, text and styling

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,10 +1,12 @@
 import MainNavigation from "./MainNavigation";
+import MainHero from "./MainHero";
 import classes from "./Layout.module.css";
 
 function Layout(props) {
     return (
         <div>
             <MainNavigation />
+            <MainHero />
             <main className={classes.main}>{props.children}</main>
         </div>
     );
---
+++ components/layout/MainHero.js
@@ -0,0 +1,19 @@
+import classes from './MainHero.module.css';
+import Image from 'next/image';
+import profilePic from '../../public/upcoming.png';
+
+function MainHero() {
+    return (
+        <div className={classes.hero}>
+            <div className={classes.upcoming}>
+                <Image
+                    src={profilePic}
+                    alt="logo"
+                />
+            </div>
+            <h1 className={classes.heroText}>Kommande premiär</h1>
+        </div>
+    );
+};
+
+export default MainHero;
