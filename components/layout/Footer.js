bc978e1a541c8f6b22c6c5698aee338a22569dce
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2022-05-10 02:54:17 +0200

    Add margin

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -3,23 +3,23 @@ import classes from "./Footer.module.css";
 function Footer() {
     return (
         <div className={classes.footer}>
-            <div className={classes.infoContent}>
-                <div className={classes.info}>
-                    <h2>Kino Ytterhogdal</h2>
-                    <p>Att se de senaste filmerna på bio är en speciell upplevelse. Luta dig tillbaka och njut av en gemensam aktivitet.</p>
-                </div>
-                <div className={classes.openingHours}>
-                    <h2>Öppettider</h2>
-                    <p>mån-tor: 11.30-23.00 </p>
-                    <p>fre-lör: 11.30 - 01.00</p>
-                    <p>sön: 14.00 - 23.00 </p>
-                </div>
-                <div className={classes.contact}>
-                    <h2>Kontakt</h2>
-                    <p>Tel: +46 123456</p>
-                    <p>Epost: bio@kino.com</p>
-                    <p>Adress: Hogdalsvägen 3 84090 Ytterhogdal, Sweden</p>
-                </div>
+            <div className={classes.info}>
+                <h2>Kino Ytterhogdal</h2>
+                <p>Att se de senaste filmerna på bio är en speciell upplevelse. Luta dig tillbaka och njut av en gemensam aktivitet.</p>
+            </div>
+            
+            <div className={classes.openingHours}>
+                <h2>Öppettider</h2>
+                <p>mån-tor: 11.30-23.00 </p>
+                <p>fre-lör: 11.30 - 01.00</p>
+                <p>sön: 14.00 - 23.00 </p>
+            </div>
+
+            <div className={classes.contact}>
+                <h2>Kontakt</h2>
+                <p>Tel: +46 123456</p>
+                <p>Epost: bio@kino.com</p>
+                <p>Adress: Hogdalsvägen 3 84090 Ytterhogdal, Sweden</p>
             </div>
 
             <div className={classes.SocialMediaContent}>
@@ -35,7 +35,7 @@ function Footer() {
                         </a>
                     </li>
                     <li className={classes.logo}>
-                        <a href="https://www.twitter.com/"> 
+                        <a href="https://www.twitter.com/">
                             <div className={classes.twitter} />
                         </a>
                     </li>
